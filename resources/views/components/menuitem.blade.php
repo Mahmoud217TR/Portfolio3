8720cb2f5e5dce842f84b989d9c9b228d73ae271
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-04-26 07:20:08 +0300

    menu added

---
+++ resources/views/components/menuitem.blade.php
@@ -0,0 +1,7 @@
+<svg id='item' width="240" height="46" viewBox="0 0 240 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M238.5 1.5V44.5H1.5V14.5162L17.6016 1.5H238.5Z" fill="#0D3C2F" stroke="#00FF95" stroke-width="3"/>
+</svg>
+<div class="dropdown-item-text">
+    {{ $text??"Menu Item" }}
+</div>
+    
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,56 +22,41 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark">
-            <div class="container">
+            <div class="container justify-self-center">
                 <a class="navbar-brand hyper-helix text-h2" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">@include('components.smallbutton',['text'=>"Login"])</a>
-                                </li>
-                            @endif
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-md-auto">
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            @include('components.button',['text'=>"Menu"])
+                        </a>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">@include('components.smallbutton',['text'=>"Register"])</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                        <div class="dropdown-menu dropdown-menu-end menu-component me-4" aria-labelledby="navbarDropdown">
+                            @auth
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                            @endauth
+                            <a class="dropdown-item" href="#">@include('components.menuitem')</a>
+                            <a class="dropdown-item" href="#">@include('components.menuitem')</a>
+                            <a class="dropdown-item" href="#">@include('components.menuitem')</a>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </nav>
 
