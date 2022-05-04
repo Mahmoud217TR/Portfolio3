026c0f899be9a32e058504cc3d0c8cc4e5af4182
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-04 05:10:47 +0300

    pagination controls

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,10 +9,26 @@
                 @include('components.projectcard')
             </div>
             @empty
-                <p class="display-4 text-light-color hyper-helix">
+                <p class="display-4 text-light-color hyper-helix text-center">
                     NO Projects added yet.
                 </p>
             @endforelse
+        </div>
+        <div class="row mt-3">
+            <div class="col d-flex justify-content-center align-items-center">
+                <!-- Previous link -->
+                @if($projects->currentPage() > 1)
+                    <a href="{{ $projects->previousPageUrl() }}">@include('components.buttons.smallbutton',['text'=>'Previous'])</a>
+                @endif
+
+                <!-- Current Page Number -->
+                <span class="pagination-indecator text-white-color mx-2 hyper-helix"> {{ $projects->currentPage() }} </span>
+
+                <!-- Next link -->
+                @if($projects->hasMorePages())
+                    <a href="{{ $projects->nextPageUrl() }}">@include('components.buttons.smallbutton',['text'=>'Next'])</a>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
