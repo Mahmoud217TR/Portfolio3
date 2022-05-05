9bd88a8fb576bfc0b4638bc18eec09527c7c1942
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-05 07:32:32 +0300

    fixed some styles

--- resources/js/components/DisplayBoardComponent.vue
+++ resources/js/components/DisplayBoardComponent.vue
@@ -5,18 +5,14 @@
                 <rect id="border" x="33.5366" y="34.9104" width="733.271" height="400.08" fill="#0F1D1E" stroke="#00FC92" stroke-width="3" stroke-dasharray="6 6"/>
                 <g id="top-right-corner">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M799.657 82.4388V0L798.97 0L786.604 0L716.531 0V13.0528L786.604 13.0528V82.4388H799.657Z" fill="#00FC92"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M799.657 82.4388V0L798.97 0L786.604 0L716.531 0V13.0528L786.604 13.0528V82.4388H799.657Z" fill="#00FC92"/>
                 </g>
                 <g id="bottom-right-corner">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M717.561 467.84L800 467.84V467.153V454.787V384.714H786.947V454.787L717.561 454.787V467.84Z" fill="#00FC92"/>
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M717.561 467.84L800 467.84V467.153V454.787V384.714H786.947V454.787L717.561 454.787V467.84Z" fill="#00FC92"/>
                 </g>
                 <g id="top-left-corner">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M82.4388 0L0 0L0 0.686963L0 13.0528L0 83.1258H13.0528L13.0528 13.0528L82.4388 13.0528V0Z" fill="#00FC92"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M82.4388 0L0 0L0 0.686963L0 13.0528L0 83.1258H13.0528L13.0528 13.0528L82.4388 13.0528V0Z" fill="#00FC92"/>
                 </g>
                 <g id="bottom-left-corner">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.343262 385.401L0.343262 467.84H1.03032V467.84H83.4691V454.787H13.3961L13.3961 385.401H0.343262Z" fill="#00FC92"/>
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M0.343262 385.401L0.343262 467.84H1.03032V467.84H83.4691V454.787H13.3961L13.3961 385.401H0.343262Z" fill="#00FC92"/>
                 </g>
             </g>
