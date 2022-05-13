788160551a81cb42b6d14118b1dd2c82c74e0993
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-14 01:09:36 +0300

    error 404 fix & error 503 made

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
-@section('title', 'Not Found')
+@section('title', 'Service Unvailable')
 
 @section('content')
     <div class="container mt-5 error-container">
         <div class="row h-100">
             <div class="col d-flex justify-content-center align-items-center">
                 <div class="display-4 text-white-color hyper-helix text-center">
-                    Error 403: Forbidden Action
+                    Error 503: Service Unvailable
                 </div>
             </div>
         </div>
