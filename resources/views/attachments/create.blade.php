98262354e406e65f57d3d72b2e372c37d56d039a
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-20 10:05:13 +0300

    done button

--- resources/views/attachments/create.blade.php
+++ resources/views/attachments/create.blade.php
@@ -2,5 +2,16 @@
 @section('title', 'Add Attachments')
 
 @section('content')
-    @include('attachments.dropzone',['url' => route('attachment.store',$project),'method'=>'post'])
+<div class="container">
+    <div class="row">
+        <div class="col">
+                @include('attachments.dropzone',['url' => route('attachment.store',$project),'method'=>'post'])
+        </div>
+    </div>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+                <a href="{{ route('project.show',$project) }}">@include('components.buttons.button',['text'=>'Done'])</a>
+        </div>
+    </div>
+</div>
 @endsection
