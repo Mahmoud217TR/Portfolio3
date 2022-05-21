b6306002200afc7920fb23b89058adfa2ae12261
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-21 07:51:30 +0300

    seeder for admin user only

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Project::factory(10)->has(Attachment::factory(3))->has(Attachment::factory()->thumb())->create()->each(function ($project){
-            $this->command->info("Created a Project with the ID: (".$project->id.")");
-        });
+        // Project::factory(10)->has(Attachment::factory(3))->has(Attachment::factory()->thumb())->create()->each(function ($project){
+        //     $this->command->info("Created a Project with the ID: (".$project->id.")");
+        // });
 
         User::factory()->admin()->create([
             'email' => 'admin@users.test'
