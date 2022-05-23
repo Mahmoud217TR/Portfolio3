67c035dcf5ae4f0ac38ee4e5fc49f2f2ade76c52
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-23 04:05:08 +0300

    about section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,7 +12,12 @@
         <div class="col-lg-6 col-md-10 mt-5">
             <h3 class="text-h3 text-light-color hyper-helix">General</h3>
             <p class="text-white-color savec mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus dolorem sed non qui asperiores aliquid tenetur molestiae doloremque, porro perspiciatis quisquam recusandae repellat eos sequi fugit ad error iusto corrupti! Repudiandae officia vel molestias, corporis quas, facilis placeat doloribus id expedita deleniti ea molestiae perferendis asperiores? Facere sed aliquam nesciunt recusandae ducimus ullam vero atque hic! Ad iure sunt corporis labore non, provident ipsam deserunt ipsum expedita quidem culpa dolore odio voluptatibus dolores, sed iste saepe pariatur aut! Est a ducimus commodi illo alias! Deleniti qui suscipit consectetur quidem dolores. Molestias ullam quae est temporibus inventore molestiae quasi assumenda? Repellendus!
+                My name is Mahmoud Mahmoud, I'm {{ date('Y')-1999 }} years old Software Engineering Student at Al-Baath University in Homs, Syria.<br>
+                I belive that I'm an Expert in Programming and Developing web & desktop apps cause I always use my abilities to the fullest trying to reach new limits, I'm also searching to Keep informed about the latest trends and developments to learn new technologies and skills so I can perfect my codes and Maximize my Skills, also I Develop creative strategies by assuming the worst and making a plan, I take calculated risks and success because I Make sound decisions in the absence of detailed instructions and informations.<br>
+                Respecting time and manage it with multiple working plans is also one of my best abilities, also I'm capable of sustaining a high level of concentration.<br>
+                I View problems as opportunities casue I learn the best from them, I always say: "I may not know what to do now, But I will make it, I always figure it out somehow and I conquer it.",<br>
+                Creating stuff is what I do, maybe it's Solution seeking, achieving an idea, learning new skill, or just filling desire.<br>
+                So if you are looking for an Extraordinary Developer I'm what you need, not because of my skills or knowledge, But for The Capabilities and Power I can show and develop fast enough to change the course of action Positively.<br>
             </p>
         </div
         >
@@ -30,7 +35,12 @@
         <div class="col-lg-6 col-md-10 mt-5">
             <h3 class="text-h3 text-light-color hyper-helix">Education</h3>
             <p class="text-white-color savec mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus dolorem sed non qui asperiores aliquid tenetur molestiae doloremque, porro perspiciatis quisquam recusandae repellat eos sequi fugit ad error iusto corrupti! Repudiandae officia vel molestias, corporis quas, facilis placeat doloribus id expedita deleniti ea molestiae perferendis asperiores? Facere sed aliquam nesciunt recusandae ducimus ullam vero atque hic! Ad iure sunt corporis labore non, provident ipsam deserunt ipsum expedita quidem culpa dolore odio voluptatibus dolores, sed iste saepe pariatur aut! Est a ducimus commodi illo alias! Deleniti qui suscipit consectetur quidem dolores. Molestias ullam quae est temporibus inventore molestiae quasi assumenda? Repellendus!
+                Beside my University learning, I'm mostly Self-taught developer who is ambitious to learn more and more about technologies and programing.<br>
+                I learned using the Internet, by taking online courses, reading documentations, watching youtube videos, and searching for solutions.<br>
+                I also have multiple Certificates:<br>
+                <ul>
+                    <li class="savec"><a href="https://www.coursera.org/account/accomplishments/specialization/certificate/N6JXVNU3PDDS">Python Specializtion</a></li>
+                </ul>
             </p>
         </div>
     </div>
@@ -38,7 +48,9 @@
         <div class="col-lg-6 col-md-10 mt-5">
             <h3 class="text-h3 text-light-color hyper-helix">Work Experinces</h3>
             <p class="text-white-color savec mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus dolorem sed non qui asperiores aliquid tenetur molestiae doloremque, porro perspiciatis quisquam recusandae repellat eos sequi fugit ad error iusto corrupti! Repudiandae officia vel molestias, corporis quas, facilis placeat doloribus id expedita deleniti ea molestiae perferendis asperiores? Facere sed aliquam nesciunt recusandae ducimus ullam vero atque hic! Ad iure sunt corporis labore non, provident ipsam deserunt ipsum expedita quidem culpa dolore odio voluptatibus dolores, sed iste saepe pariatur aut! Est a ducimus commodi illo alias! Deleniti qui suscipit consectetur quidem dolores. Molestias ullam quae est temporibus inventore molestiae quasi assumenda? Repellendus!
+                Most of my projects are personal, made to fix an issue that I encounterd in real life.<br>
+                I also made several freelance projects.<br>
+                I participated in the translation of laravel framework documentaion.<br>
             </p>
         </div>
     </div>
