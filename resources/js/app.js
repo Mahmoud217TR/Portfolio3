71b58805f16ac93672574e02f5a0c98efe16d85c
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-05-31 14:55:54 +0300

    fixing vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,8 @@ import { createApp } from 'vue';
 import CKEditor from '@ckeditor/ckeditor5-vue';
 
 let app=createApp({}).use(CKEditor)
-app.component('display-component', require('./components/DisplayBoardComponent.vue').default);
-app.component('editor-component', require('./components/EditorComponent.vue').default);
-app.component('dropzone-component', require('./components/DropZoneComponent.vue').default);
-app.component('contact-component', require('./components/ContactComponent.vue').default);
+app.component('display-component', require('./components/DisplayBoardComponent.vue'));
+app.component('editor-component', require('./components/EditorComponent.vue'));
+app.component('dropzone-component', require('./components/DropZoneComponent.vue'));
+app.component('contact-component', require('./components/ContactComponent.vue'));
 app.mount("#app");
